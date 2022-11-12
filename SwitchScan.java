cb3ee7f00ff13659276305aeaa97f7debd7eb4b2
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-11-12 00:04:44 -0600

    if statement vs switch statement

---
+++ SwitchScan.java
@@ -0,0 +1,18 @@
+int number = 1;
+switch (number) {
+  case 0:
+    System.out.println("The number is 0");
+    break;
+  case 1:
+    System.out.println("The number is 1");
+    break;
+  case 2:
+    System.out.println("The number is 2");
+    break;
+  case 3:
+    System.out.println("The number is 3");
+    break;
+  default:
+    System.out.println("I don't recognize the number");
+    break;
+}
