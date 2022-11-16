895538b018eb8647239ac5b16a49d31301115641
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-11-15 22:39:07 -0600

    block scooping in java

---
+++ Scoop/Scoop.java
@@ -0,0 +1,10 @@
+class Main {
+  // Static variable that you can access wherever you want in your code 
+  static String stc = "Class/Static Scoop"
+
+  public static void main(String[] args) {  
+    System.out.println(stc);
+    System.out.println(Main.stc);
+  }
+
+}
